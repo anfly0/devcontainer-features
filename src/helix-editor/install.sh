7c8579ae32bd049d076714b704d8cbd6cea7601d
anfly0 <anfly0@gmail.com>
2025-06-07 19:27:12 +0200

    Redo helix install

--- src/helix-editor/install.sh
+++ src/helix-editor/install.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 set -euo pipefail
 
-ls -l
+source /etc/os-release
 
-# Extract and find extracted directory
-extracted_dir=$(find . -maxdepth 1 -type d -name "helix-*-x86_64-linux" | head -n 1)
+deps_install() {
+  echo "Installing dependencies..."
+  case "${ID}" in
+  debian|ubuntu)
+    apt-get update -y && apt-get -y install --no-install-recommends curl tar xz-utils ca-certificates
+    ;;
+  *)
+    echo "OS not supported.." 
+    exit 1
+  esac
+}
 
-if [[ -z "$extracted_dir" ]]; then
-  echo "Failed to find extracted Helix directory."
-  exit 1
-fi
 
-# Install binaries
-mv "$extracted_dir/hx" /usr/local/bin/
-mv "$extracted_dir/runtime" /usr/local/bin/
 
-# Cleanup
-rm -rf "$extracted_dir"
 
-echo "Helix installed successfully."
+helix_install() {
+
+  VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/helix-editor/helix/releases/latest | grep -o '[^/]*$')
+
+  curl -L "https://github.com/helix-editor/helix/releases/download/${VERSION}/helix-${VERSION}-x86_64-linux.tar.xz" -o helix.tar.xz
+
+  tar -xf helix.tar.xz
+
+  extracted_dir=$(find . -maxdepth 1 -type d -name "helix-*-x86_64-linux" | head -n 1)
+
+  if [[ -z "$extracted_dir" ]]; then
+    echo "Failed to find extracted Helix directory."
+    exit 1
+  fi
+
+  mv "$extracted_dir/hx" /usr/local/bin/
+  mv "$extracted_dir/runtime" /usr/local/bin/
+
+  rm -rf "$extracted_dir"
+
+  echo "Helix installed successfully."
+}
+
+deps_install
+helix_install
